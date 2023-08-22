3d3ed31af76c4bfb50fbe220a3b0b0a58ff38cea
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2023-08-22 13:30:24 -0600

    feat(footer): make footer template

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -2,5 +2,9 @@ window.onload = () => {
     fetch("/_assets/template/navi.html")
     .then(response => response.text())
     .then(text => document.getElementById("navi-links").innerHTML = text);
+
+    fetch("/_assets/template/footer.html")
+    .then(response => response.text())
+    .then(text => document.getElementById("footer").innerHTML = text);
 }
 
