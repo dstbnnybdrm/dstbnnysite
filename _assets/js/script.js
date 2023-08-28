d686509c3738694bd1e85b173295e883bed74406
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2023-08-27 21:33:13 -0600

    chore(style): update other pages to new methodology and layout

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,10 +1,10 @@
 window.onload = () => {
     fetch("/_assets/template/navi.html")
     .then(response => response.text())
-    .then(text => document.getElementById("navi-links").innerHTML = text);
+    .then(text => document.getElementById("js-navi-list").innerHTML = text);
 
     fetch("/_assets/template/footer.html")
     .then(response => response.text())
-    .then(text => document.getElementById("footer").innerHTML = text);
+    .then(text => document.getElementById("js-footer").innerHTML = text);
 }
 
