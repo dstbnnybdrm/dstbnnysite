63d81bb80721a565c840c4f242ed7604153ea2a2
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2023-09-11 20:59:41 -0600

    feat: add prettier formatter

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,10 +1,14 @@
 window.onload = () => {
     fetch("/_assets/template/navi.html")
-    .then(response => response.text())
-    .then(text => document.getElementById("js-navi-list").innerHTML = text);
+        .then((response) => response.text())
+        .then(
+            (text) =>
+                (document.getElementById("js-navi-list").innerHTML = text),
+        );
 
     fetch("/_assets/template/footer.html")
-    .then(response => response.text())
-    .then(text => document.getElementById("js-footer").innerHTML = text);
-}
-
+        .then((response) => response.text())
+        .then(
+            (text) => (document.getElementById("js-footer").innerHTML = text),
+        );
+};
