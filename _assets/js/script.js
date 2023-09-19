15c581292f52e46120c79a01ccdc10dfe82812f2
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2023-09-18 20:46:01 -0600

    fix(script): stop trying to load nav-list everywhere
    now checks if id exists in page before trying to load external html

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,14 +1,25 @@
-window.onload = () => {
-    fetch("/_assets/template/navi.html")
-        .then((response) => response.text())
-        .then(
-            (text) =>
-                (document.getElementById("js-navi-list").innerHTML = text),
-        );
+var url = window.location.pathname;
 
-    fetch("/_assets/template/footer.html")
-        .then((response) => response.text())
-        .then(
-            (text) => (document.getElementById("js-footer").innerHTML = text),
-        );
+var navi_list_id = document.getElementById("js-navi-list");
+var new_stuff_id = document.getElementById("js-new-stuff");
+var footer_id = document.getElementById("js-footer");
+
+window.onload = () => {
+    if (navi_list_id) {
+        fetch("/_assets/template/navi.html")
+            .then((response) => response.text())
+            .then((text) => (navi_list_id.innerHTML = text));
+    }
+
+    if (new_stuff_id) {
+        fetch("/_assets/template/new.html")
+            .then((response) => response.text())
+            .then((text) => (new_stuff_id.innerHTML = text));
+    }
+
+    if (footer_id) {
+        fetch("/_assets/template/footer.html")
+            .then((response) => response.text())
+            .then((text) => (footer_id.innerHTML = text));
+    }
 };
