a7c9511f9c38fdb2ad956d45122fc46608cd5b6f
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2023-09-21 13:41:08 -0600

    feat(comms): create and add commission status
    is a template, so can be added anywhere and edited in one place

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -3,6 +3,7 @@ var url = window.location.pathname;
 var navi_list_id = document.getElementById("js-navi-list");
 var new_stuff_id = document.getElementById("js-new-stuff");
 var footer_id = document.getElementById("js-footer");
+var comm_status_id = document.getElementById("js-comm-status");
 
 window.onload = () => {
     if (navi_list_id) {
@@ -21,5 +22,11 @@ window.onload = () => {
         fetch("/_assets/template/footer.html")
             .then((response) => response.text())
             .then((text) => (footer_id.innerHTML = text));
+    }
+
+    if (comm_status_id) {
+        fetch("/_assets/template/comm-status.html")
+            .then((response) => response.text())
+            .then((text) => (comm_status_id.innerHTML = text));
     }
 };
