d1e1d9f726fb52085ae01048a257c56c24d5830a
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2023-11-19 23:33:50 -0700

    refactor: run prettier

--- public/_assets/js/script.js
+++ public/_assets/js/script.js
@@ -1,29 +1,29 @@
 const template_url = "/_assets/template/";
 const id_names = [
-    "js-navi-list",
-    "js-new-stuff",
-    "js-footer",
-    "js-comm-status",
-    "js-update-log",
-    "js-88x31",
+  "js-navi-list",
+  "js-new-stuff",
+  "js-footer",
+  "js-comm-status",
+  "js-update-log",
+  "js-88x31",
 ];
 
 window.onload = () => {
-    id_names.forEach((name) => {
-        let id = document.getElementById(name);
+  id_names.forEach((name) => {
+    let id = document.getElementById(name);
 
-        if (!id) {
-            // id wasn't found on this page so move on
-            return;
-        }
+    if (!id) {
+      // id wasn't found on this page so move on
+      return;
+    }
 
-        // get filename from id name
-        let template_filename = name.substring(3).concat(".html");
-        let template_location = template_url.concat(template_filename);
+    // get filename from id name
+    let template_filename = name.substring(3).concat(".html");
+    let template_location = template_url.concat(template_filename);
 
-        // load template into page
-        fetch(template_location)
-            .then((response) => response.text())
-            .then((content) => (id.innerHTML = content));
-    });
+    // load template into page
+    fetch(template_location)
+      .then((response) => response.text())
+      .then((content) => (id.innerHTML = content));
+  });
 };
