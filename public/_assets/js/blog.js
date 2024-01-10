db9e4f358bae3f88886148eb6af35bbd7d8c9f70
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2024-01-10 16:10:14 -0700

    fix(blog): iframe height + wrong date on post

--- public/_assets/js/blog.js
+++ public/_assets/js/blog.js
@@ -1,21 +1,27 @@
-// some code below taken and modified from lostlove's blog found here:
+// some code below modified from lostlove's blog which can be found here:
 // https://lostlove.neocities.org/blog
 
-const post_iframe = document.getElementById("js-blog-post");
-
+const blog_post = document.getElementById("js-blog-post");
 let post_buttons = document.querySelectorAll(".posts-list__button");
 
 // add click event to every post button
 post_buttons.forEach(function (button) {
   button.addEventListener("click", function () {
     // change iframe to button's post url
-    let post_url = button.getAttribute("data-post-url");
-    post_iframe.src = post_url;
+    let post_location = button.getAttribute("data-post-url");
+    blog_post.src = post_location;
   });
 });
 
-// show most recent post on load
+// show most recent post on page load
 window.onload = () => {
-  let recent_post_url = post_buttons[0].getAttribute("data-post-url");
-  post_iframe.src = recent_post_url;
+  let recent_post_location = post_buttons[0].getAttribute("data-post-url");
+  blog_post.src = recent_post_location;
+};
+
+// set height of frame to height of the post's contents
+blog_post.onload = () => {
+  let blog_post_content = blog_post.contentWindow;
+  blog_post.height = ""; // reset height first
+  blog_post.height = blog_post_content.document.body.scrollHeight + "px";
 };
