230525ffc8a8a40c0fc651c05fc33a8bdc732d29
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2024-01-15 23:20:26 -0700

    feat(layout)!: make main content window an iframe

--- public/_assets/js/script.js
+++ public/_assets/js/script.js
@@ -1,31 +1,26 @@
-const template_url = "/_assets/template/";
-const id_names = [
-  "js-header",
-  "js-navi",
-  "js-status",
-  "js-collection",
-  "js-footer",
-  "js-comm-status",
-  "js-news-feed",
-  "js-update-log",
-];
+function resize_iframe(iframe) {
+  iframe.height = "";
+  iframe.height = iframe.contentWindow.document.body.scrollHeight + "px";
+  console.log(iframe.height);
+}
 
-window.onload = () => {
-  id_names.forEach((name) => {
-    let id = document.getElementById(name);
+// window.onload = () => {
+//   id_names.forEach((name) => {
+//     let id = document.getElementById(name);
 
-    if (!id) {
-      // id wasn't found on this page so move on
-      return;
-    }
+//     if (!id) {
+//       // id wasn't found on this page so move on
+//       return;
+//     }
 
-    // get filename from id name
-    let template_filename = name.substring(3).concat(".html");
-    let template_location = template_url.concat(template_filename);
+//     // get filename from id name
+//     let template_filename = name.substring(3).concat(".html");
+//     let template_location = template_url.concat(template_filename);
 
-    // load template into page
-    fetch(template_location)
-      .then((response) => response.text())
-      .then((content) => (id.innerHTML = content));
-  });
-};
+//     // load template into page
+//     fetch(template_location)
+//       .then((response) => response.text())
+//       .then((content) => (id.innerHTML = content));
+//   });
+// };
+//
