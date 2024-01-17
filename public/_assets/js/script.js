d035815492f8a3fdc4ffb9977284e4e024c47efe
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2024-01-16 22:23:33 -0700

    feat(header): randomise header tagline on load

--- public/_assets/js/script.js
+++ public/_assets/js/script.js
@@ -1,4 +1,6 @@
 const frame_element = document.getElementsByName("mainframe")[0];
+const tagline_element = document.getElementById("tagline");
+const taglines_list = ["enjoy yr stay"];
 
 function update_content_height() {
   if (frame_element == null) {
@@ -9,8 +11,16 @@ function update_content_height() {
   frame_element.height = frame_content.document.body.scrollHeight + "px";
 }
 
+function randomise_tagline() {
+  let index = Math.floor(Math.random() * taglines_list.length);
+  let random_tagline = taglines_list[index];
+
+  tagline.textContent = random_tagline;
+}
+
 window.onload = () => {
   update_content_height();
+  randomise_tagline();
 };
 
 // dynamically resize frame height
