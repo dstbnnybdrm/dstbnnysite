2a6612c8553edaaf4af4ac39c072e1e3ab902eeb
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2024-01-17 20:11:54 -0700

    feat(home): add script to load update and news

---
+++ public/_assets/js/home.js
@@ -0,0 +1,15 @@
+const ids_list = ["news-feed", "update-log"];
+
+window.onload = () => {
+  ids_list.forEach((id) => {
+    let element = document.getElementById(id);
+    let element_url = "_assets/template/" + id + ".html";
+    console.log(element_url);
+
+    fetch(element_url)
+      .then((response) => response.text())
+      .then((html) => {
+        element.innerHTML = html;
+      });
+  });
+};
