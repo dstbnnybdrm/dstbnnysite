e4bd4775b107a124ae49e6a3d327d1f07ea1f5b9
dstbnnybdrm <dustbunnybedroommusic@gmail.com>
2024-01-30 15:22:05 -0700

    refactor(blog): rename post frame
    + use event listeners
    + extrapolate button code to func

--- public/_assets/js/blog.js
+++ public/_assets/js/blog.js
@@ -1,27 +1,37 @@
-// some code below modified from lostlove's blog which can be found here:
-// https://lostlove.neocities.org/blog
+const postFrame = document.getElementByName("post-frame")[0];
+const postNavButtons = document.querySelectorAll(".posts-list__button");
 
-const blog_post = document.getElementById("js-blog-post");
-let post_buttons = document.querySelectorAll(".posts-list__button");
+function updateContentHeight() {
+    if (postFrame == null) {
+        console.error("couldn't find frame");
+        return;
+    }
+
+    const frameContent = postFrame.contentWindow;
+
+    postFrame.height = "0";
+    postFrame.height = frameContent.document.body.scrollHeight + "px";
+
+    console.log("content height updated: " + postFrame.height);
+}
+
+// (modified from lostlove's blog which can be found here:
+// https://lostlove.neocities.org/blog)
+function linkNavButtons() {
+    postNavButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            // change iframe to button's post url
+            let post_location = button.getAttribute("data-post-url");
+            blog_post.src = post_location;
+        });
+    });
+}
 
 // add click event to every post button
-post_buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        // change iframe to button's post url
-        let post_location = button.getAttribute("data-post-url");
-        blog_post.src = post_location;
-    });
-});
+window.addEventListener("load", linkNavButtons);
 
 // show most recent post on page load
-window.onload = () => {
-    let recent_post_location = post_buttons[0].getAttribute("data-post-url");
+window.addEventListener("load", function () {
+    let recent_post_location = postNavButtons[0].getAttribute("data-post-url");
     blog_post.src = recent_post_location;
-};
-
-// set height of frame to height of the post's contents
-blog_post.onload = () => {
-    let blog_post_content = blog_post.contentWindow;
-    blog_post.height = ""; // reset height first
-    blog_post.height = blog_post_content.document.body.scrollHeight + "px";
-};
+});
