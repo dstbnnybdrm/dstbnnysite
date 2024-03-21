6ee672e05795f1815b79fc8acbcb49041e5b0e5e
dstbnnybdrm <dustbunny.bedroom@gmail.com>
2024-03-21 15:17:38 -0600

    docs(theme): comment IIEF for setting theme

--- public/_assets/js/set-theme.js
+++ public/_assets/js/set-theme.js
@@ -1,9 +1,16 @@
+/**
+ * render-blocking function to load a cached theme, if it exists, ASAP on page
+ * load.
+ *
+ * mostly borrowed from:
+ *   https://www.aleksandrhovhannisyan.com/blog/the-perfect-theme-switch/
+ *
+ */
 (() => {
     const ROOT = document.documentElement;
     const THEME_KEY = "theme";
     const cachedTheme = localStorage.getItem(THEME_KEY);
     if (cachedTheme) {
-        console.log("using cached theme: " + cachedTheme);
         ROOT.dataset[THEME_KEY] = cachedTheme;
-    } else console.log("using system theme");
+    }
 })();
