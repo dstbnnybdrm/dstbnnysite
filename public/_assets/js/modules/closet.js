5b91a1010a78d32dc2cf7ba116dc6ba7dcec807c
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2024-06-09 16:19:02 -0600

    feat(closet): update conditions for padlock comb.

--- public/_assets/js/modules/closet.js
+++ public/_assets/js/modules/closet.js
@@ -13,10 +13,21 @@ export function promptCombination() {
 
     let userInput = prompt("ENTER THE PADLOCK COMBINATION:");
     switch (userInput) {
+        // user cancels
+        case null:
+            alert("I'LL STILL BE HERE.");
+            break;
+        // user inputs nothing
+        case "":
+            alert("UH, YOU DIDN'T TYPE ANYTHING. TRY AGAIN.");
+            promptCombination();
+            break;
+        // user is correct
         case PADLOCK_COMBINATION:
             localStorage.setItem("padlockUnlocked", "true");
             enterCloset();
             break;
+        // user is wrong
         default:
             alert("INCORRECT COMBINATION.");
             break;
